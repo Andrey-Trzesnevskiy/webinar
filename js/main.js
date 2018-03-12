e2fe4ef8110f4e0dce56106baa7fb8053b411ede
Andrey-Trzesnevskiy <rockman06@mail.ru>
2018-03-12 22:11:25 +0200

    script for getting params for url was fixed

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 window.onload = function () {
     let queryString = document.location.search;
-    let params = queryString.substring(1);
-    let index = params.indexOf("&");
-    let pid = params.substring(0, index);
-    let a = pid.indexOf("=");
-    pid = +pid.substring(a+1);
-    let lid = params.substring(index+1);
-    let b = lid.indexOf("=");
-    lid = +lid.substring(b+1);
+    queryString = queryString.replace('?','').split('&');
+    let obj = queryString.reduce(
+        function(p,curr){
+            let aa = curr.split('=');
+            p[decodeURIComponent(aa[0])] = decodeURIComponent(aa[1]);
+            return p;
+        },
+        {}
+    )
     let inputs = document.querySelectorAll('.hidden-input input');
-    inputs[0].value = pid;
-    inputs[1].value = lid;
+    inputs[0].value = obj.pid;
+    inputs[1].value = obj.lid;
 }
